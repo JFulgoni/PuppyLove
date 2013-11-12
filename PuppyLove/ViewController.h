458a1d0441d0c5d87e2008904d5e7c04c5f2201c
John Fulgoni <johnfulgoni@Johns-MacBook-Air.local>
2013-11-12 10:54:10 -0500

    Initial Commit

---
+++ PuppyLove/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PuppyLove
+//
+//  Created by John Fulgoni on 11/12/13.
+//  Copyright (c) 2013 John Fulgoni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ PuppyLove/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PuppyLove
+//
+//  Created by John Fulgoni on 11/12/13.
+//  Copyright (c) 2013 John Fulgoni. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
